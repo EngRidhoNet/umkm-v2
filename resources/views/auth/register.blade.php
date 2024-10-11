2b2721577bd6071e2206eed1cda3e557b4bd46e6
Ridho Aulia Rahman <144804261+EngRidhoNet@users.noreply.github.com>
2024-10-12 06:52:42 +0700

    vite

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -120,7 +120,7 @@
                     </div>
                     <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Buat Akun</button>
                     <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                        Sudah Punya Akun? <a href="{{ route('login') }}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login Disini</a>
+                    Sudah Punya Akun? <a href="{{ route('login') }}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login Disini</a>
                     </p>
                 </form>
             </div>
